91067cc07789eb99a89dcb62d06e09fe16f2c7e4
webcrawls <github.com@kaden.sh>
2023-07-09 07:06:52 -0400

    💄(layout) move page-nav into header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,17 @@
 <main>
     <header>
         <h1>/home/webcrawls</h1>
+        <nav class="page-nav">
+            <p>~</p>
+            <ul>
+                <li><a href="/">index</a></li>
+                <li><a href="/about">about</a></li>
+                <li><a href="/projects">projects</a></li>
+                <!--            <li><a href="/technology">technology</a></li>-->
+            </ul>
+            <p>~</p>
+        </nav>
     </header>
-    <nav class="page-nav">
-        <p>~</p>
-        <ul>
-            <li><a href="/">index</a></li>
-            <li><a href="/about">about</a></li>
-            <li><a href="/projects">projects</a></li>
-<!--            <li><a href="/technology">technology</a></li>-->
-        </ul>
-        <p>~</p>
-    </nav>
     <section>
         <slot></slot>
     </section>
