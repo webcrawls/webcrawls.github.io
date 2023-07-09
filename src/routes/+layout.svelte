ad16c9c06dba2734f09c85f8e7d1351d64f3ad58
webcrawls <github.com@kaden.sh>
2023-07-09 04:18:57 -0400

    💄 improve root layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,17 @@
-<header>
-    <h1>/home/webcrawls</h1>
-</header>
-<nav class="page-nav">
-    <p>~</p>
-    <ul>
-        <li><a href="/">index</a></li>
-        <li><a href="/about">about</a></li>
-        <li><a href="/technology">technology</a></li>
-    </ul>
-    <p>~</p>
-</nav>
 <main>
-    <slot></slot>
+    <header>
+        <h1>/home/webcrawls</h1>
+    </header>
+    <nav class="page-nav">
+        <p>~</p>
+        <ul>
+            <li><a href="/">index</a></li>
+            <li><a href="/about">about</a></li>
+            <li><a href="/technology">technology</a></li>
+        </ul>
+        <p>~</p>
+    </nav>
+    <section>
+        <slot></slot>
+    </section>
 </main>
