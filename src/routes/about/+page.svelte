c40ba4e152b61f9af7b52b3745239b6ccf247316
webcrawls <github.com@kaden.sh>
2023-07-09 07:12:56 -0400

    💄(about) update content

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,8 +6,17 @@
     <h1>about me</h1>
 </PageTitle>
 <article>
-    <p>hello! I'm Kaden, or usually <b>@webcrawls</b> online.</p>
+    <p>Hello! I'm Kaden, or usually <b>@webcrawls</b> online.</p>
     <p>I'm 19, from :ca:, and love all sorts of creative endeavours.</p>
-    <p>I also like to draw, make music, and pretty much anything else that interests me.</p>
-    <p>if you're looking to message me, send an email at "contact (at) kaden.sh" :)</p>
+    <p>I spend most of my time coding, and I'm currently pursuing higher education as a software engineer.</p>
+    <p>You can read about some of my projects <a href="/projects">here</a>.</p>
 </article>
+<h1>~</h1>
+<article>
+    <p>If you're looking to contact me, send me a message one one of the following platforms. I'll do my best to respond
+        quickly :)</p>
+</article>
+<PageTitle>
+    <h3>discord: @webcrawls</h3>
+    <h3>mail: contact at kaden.🤫</h3>
+</PageTitle>
